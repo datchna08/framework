3df3d6faa0718ab7cd908298d60a6250e0472542
datchna08 <datchnam@gmail.com>
2026-09-18 19:00:16 +0530

    Add S11 test data management

---
+++ test-data/studentData.js
@@ -0,0 +1,9 @@
+const studentData = {
+    firstname : 'john',
+    lastname : 'deo',
+    email : 'john.deo@gmail.com',
+    gender : "Male",
+    mobile : '9876543210'
+}
+
+module.exports = studentData;
